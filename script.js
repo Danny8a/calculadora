ddd1afba1bbc7b5ec06b9c031b93bd2a514eb282
Danny8a <46731988+Danny8a@users.noreply.github.com>
2025-10-23 09:15:09 -0500

    Script ajustado

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("#button");
 
 buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id === "clear") {
-      display.innerText = "";
-    } else if (item.id === "backspace") {
-      let string = display.innerText.toString();
-        display.innerText = string.slice(0, -1);
-    }
-  };
+    item.onclick = () => {
+        if (item.id === "clear") {
+            display.innerText = "";
+        } else if (item.id === "backspace") {
+            let string = display.innerText.toString();
+            display.innerText = string.slice(0, -1);
+        } else if (display.innerText !== "" && item.id === "equal") {
+            // let cadena ="2+2"
+            display.innerText = eval(display.innerText)
+        } else if (display.innerText === "" && item.id === "equal") {
+            display.innerText = "Null";
+            setTimeout(() => (display.innerText = ""), 2000);
+
+        } else {
+            display.innerText += item.id;
+        }
+    };
 });
